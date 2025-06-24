a586424a1ca1d00dec0e5f3371614a0f3b2ca5cc
Aman Chaurasiya <aryansim1230@gmail.com>
2025-06-24 16:03:29 +0530

    added Backend

---
+++ Backend/models/Expense.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const ExpenseSchema = new mongoose.Schema({
+  label: { type: String, required: true },
+  value: { type: Number, required: true },
+  date: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Expense", ExpenseSchema);
