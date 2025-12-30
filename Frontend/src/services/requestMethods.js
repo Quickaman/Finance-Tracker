d8345d2ebc6afabd234f5328132f7570537d5602
Aman Chaurasiya <148737365+Quickaman@users.noreply.github.com>
2025-12-30 17:49:04 +0530

    Update BASE_URL to use environment variable

--- Frontend/src/services/requestMethods.js
+++ Frontend/src/services/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
+;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
